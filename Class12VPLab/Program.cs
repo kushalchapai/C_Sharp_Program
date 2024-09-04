41142966d7157cf73889121110428e99e690dbcf
kushalchapai <141694466+kushalchapai@users.noreply.github.com>
2024-09-04 11:56:16 +0545

    Add files via upload

---
+++ Class12VPLab/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+namespace Class12VPLab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /* 
+            Q1 ob1 = new Q1();
+            ob1.Output
+            */
+
+            /*
+            Q2 ob2 = new Q2();
+            double sum = ob2.Addition(7,6);
+            Console.WriteLine("The sum is: "+sum); 
+            */
+
+            /*
+            Q3 ob3 = new Q3();
+            ob3.Division();
+            */
+
+
+            //Q4.Operation1();
+
+            //Q5.Swap();
+
+            //Q6 ob6 = new Q6();
+            //ob6.Multiplication
+
+            //Q7 q7 = new Q7();
+            //q7.Arithmetic();
+
+            //Q8.Reverse();
+
+            Q9.Cicle();
+        }
+    }
+
+}
