3c849ef24c68653ebc072d50b732b3aee06381f1
kushalchapai <141694466+kushalchapai@users.noreply.github.com>
2024-09-04 11:48:00 +0545

    Add files via upload

---
+++ Class12VPLab/Q8.cs
@@ -0,0 +1,20 @@
+﻿
+//8. Write a C# Sharp program that takes three letters and displays them in reverse order.
+using System;
+
+namespace Class12VPLab
+{
+    public class Q8
+    {
+        public static void Reverse()
+        {
+            Console.WriteLine("Input three letters: ");
+
+            char letter = Convert.ToChar(Console.ReadLine());
+            char letter1 = Convert.ToChar(Console.ReadLine());
+            char letter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", letter2, letter1, letter);
+        }
+    }
+}
