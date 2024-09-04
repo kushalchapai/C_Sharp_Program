3d77513e17c8fdf54fbb953749891465762302d8
kushalchapai <141694466+kushalchapai@users.noreply.github.com>
2024-09-04 11:54:50 +0545

    Add files via upload

---
+++ Class12VPLab/Q9.cs
@@ -0,0 +1,22 @@
+﻿//9. Write a C# Sharp program that takes the radius of a circle as input and
+//calculates the perimeter and area of the circle.
+
+using System;
+
+namespace Class12VPLab
+{
+    public class Q9
+    {
+        public static void Cicle()
+        {
+            Console.Write("Enter the radius of circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double perimeter = 2 * 3.14 * radius;
+            double area = 3.14 * radius * radius;
+
+            Console.WriteLine("Perimeter of circle: " + perimeter);
+            Console.WriteLine("Area of circle: " + area);
+        }
+    }
+}
